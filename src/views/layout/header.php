c149cb3264f9cf2656faacb0853ee324b60a1a2c
Matthijs Hagenaars <mphagenaars@users.noreply.github.com>
2025-12-28 17:41:22 +0100

    fix: add cache busting to css link

--- src/views/layout/header.php
+++ src/views/layout/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $pageTitle ?? 'Trainer Bobby' ?></title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/style.css?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'] . '/css/style.css') ?>">
 </head>
 <body>
 <header class="main-header">
